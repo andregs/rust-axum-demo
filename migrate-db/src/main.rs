4dc8aeb8e25221916e402d0f228d9058be228dba
André Gomes <andregs@users.noreply.github.com>
2022-04-30 12:02:59 +0200

    improve migrations logging
    Signed-off-by: André Gomes <andregs@users.noreply.github.com>

--- migrate-db/src/main.rs
+++ migrate-db/src/main.rs
@@ -1,24 +1,30 @@
-use std::time::Duration;
-
 use sqlx::postgres::{PgConnectOptions, PgConnection};
 use sqlx::{ConnectOptions, Connection};
-use tracing::info_span;
-use utils::config;
+use std::time::Duration;
+use tracing::info;
+use utils::config::{app, Config};
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-    let app_config = config::app::configure();
+    migrate(app::configure()).await
+}
 
-    let mut db_config: PgConnectOptions = app_config.database_url.parse()?;
+#[tracing::instrument(
+    skip(cfg),
+    fields(%cfg.hostname),
+)]
+async fn migrate(cfg: Config) -> Result<(), sqlx::Error> {
+    let mut db_config: PgConnectOptions = cfg.database_url.parse()?;
     db_config.disable_statement_logging();
 
     let db = &mut PgConnection::connect_with(&db_config).await?;
-    info_span!("Migration", "Migrating DB...");
-    sqlx::migrate!().run(db).await?;
+    info!("Migrating DB...");
 
+    // faking some long-running migrations here
     // TODO web app can only be ready after migrations are done
-    tokio::time::sleep(Duration::from_secs(12)).await; // remove me
+    tokio::time::sleep(Duration::from_secs(7)).await; // remove me
 
-    info_span!("Migration", "DB Migrated.");
+    sqlx::migrate!().run(db).await?;
+    info!("DB Migrated.");
     Ok(())
 }
